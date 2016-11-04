99ef700a7d2e0b011b3e62c48ed730fa0b8c5b28
Michael Habib <michaelhabib1868@gmail.com>
2016-11-03 20:53:09 -0700

    Add quitting functionality for application

---
+++ Assets/Rollingball/Scripts/QuitOnAndroidBack.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace com.mh.physsim.rollingball
+{
+    public sealed class QuitOnAndroidBack : MonoBehaviour
+    {
+
+        void Update ()
+        {
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+}
+
+
